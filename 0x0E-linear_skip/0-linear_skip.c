2f4a816ecde7898040ee40fe51990f2c0a18646a
didierrevelo <1829@holbertonschool.com>
2021-11-03 13:36:27 -0500

    0-linear_skip.c

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -8,43 +8,44 @@
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-    skiplist_t *current = list;
-    skiplist_t *express_lane = list;
-    size_t i = 0;
+	skiplist_t *current = list;
+	skiplist_t *express_lane = list;
+	size_t i = 0;
 
-    if (!list)
+	if (!list)
 		return (NULL);
 
 
-    while (current && current->next && (current->n) < value)
-    {
-        express_lane = current;
-        if (current->express)
-        {
-            current = current->express;
-            printf("Value checked at index [%lu] = [%d]\n", current->index, current->n);
-        }
-        else
-        {
-            while (current->next){
-                current = current->next;
-            }
-            
-        }
-    }
-    printf("Value found between indexes [%lu] and [%lu]\n", express_lane->index, current->index);
+	while (current && current->next && (current->n) < value)
+	{
+		express_lane = current;
+		if (current->express)
+		{
+			current = current->express;
+			printf("Value checked at index [%lu] = [%d]\n", current->index, current->n);
+		}
+		else
+		{
+			while (current->next)
+			{
+				current = current->next;
+			}
+		}
+	}
+	printf("Value found between indexes [%lu] and [%lu]\n",
+					 express_lane->index, current->index);
 
-    for (i = express_lane->index;
-                     i <= (current->index) && (express_lane->n <= value);
-                     i++, express_lane = express_lane->next)
-    {
-        printf("Value checked at index [%lu] = [%d]\n", i, express_lane->n);
-        if (express_lane && (express_lane->n) == value)
-            return (express_lane);
-    }
-    if (express_lane)
-    {
-        printf("Value checked at index [%lu] = [%d]\n", i, express_lane->n);
-    }
-    return (NULL);
+	for (i = express_lane->index;
+					 i <= (current->index) && (express_lane->n <= value);
+					 i++, express_lane = express_lane->next)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", i, express_lane->n);
+		if (express_lane && (express_lane->n) == value)
+			return (express_lane);
+	}
+	if (express_lane)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", i, express_lane->n);
+	}
+	return (NULL);
 }
