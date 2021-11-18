f23086c63e6f0ddecb111f3bda572bc9bad07eb5
didierrevelo <1829@holbertonschool.com>
2021-11-18 12:13:40 -0500

    0-rain.py

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -2,7 +2,12 @@
 
 
 def rain(walls):
-    """
+    """Given a list of non-negative
+    integers representing the heights
+    of walls with unit width 1, as if
+    viewing the cross-section of a relief
+    map, calculate how many square units
+    of water will be retained after it rains.
     """
     if not walls or not isinstance(walls, list):
         return 0
