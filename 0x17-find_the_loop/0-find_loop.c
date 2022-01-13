1876779f874bc03fd3c9409a5a5415cec9cfc27f
didierrevelo <1829@holbertonschool.com>
2022-01-12 19:45:44 -0500

    0-find_loop.c

--- 0x17-find_the_loop/0-find_loop.c
+++ 0x17-find_the_loop/0-find_loop.c
@@ -9,26 +9,26 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-    listint_t *p1, *p2;
+	listint_t *p1, *p2;
 
-    if (head == NULL)
-        return (NULL);
-    p1 = head;
-    p2 = head;
-    while (p2 && p2->next)
-    {
-        p1 = p1->next;
-        p2 = p2->next->next;
-        if (p1 == p2)
-        {
-            p1 = head;
-            while (p1 != p2)
-            {
-                p1 = p1->next;
-                p2 = p2->next;
-            }
-            return (p1);
-        }
-    }
-    return (NULL);
+	if (head == NULL)
+	return (NULL);
+	p1 = head;
+	p2 = head;
+	while (p2 && p2->next)
+	{
+		p1 = p1->next;
+		p2 = p2->next->next;
+		if (p1 == p2)
+		{
+			p1 = head;
+			while (p1 != p2)
+			{
+				p1 = p1->next;
+				p2 = p2->next;
+			}
+			return (p1);
+			}
+		}
+	return (NULL);
 }
