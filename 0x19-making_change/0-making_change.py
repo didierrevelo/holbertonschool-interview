4a6651d287a592903b2cf27c31081e4ddcde64a1
didierrevelo <1829@holbertonschool.com>
2022-02-02 10:46:23 -0500

    0. Change comes from within

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -19,12 +19,12 @@ def makeChange(coins, total):
     for i in range(1, total + 1):
         memory[i] = placeholder
 
-    for coin in coins:
-        current = i - coin
-        if current < 0:
-            continue
+        for coin in coins:
+            current = i - coin
+            if current < 0:
+                continue
 
-    memory[i] = min(memory[current] + 1, memory[i])
+            memory[i] = min(memory[current] + 1, memory[i])
 
     if memory[total] == total + 1:
         return -1
