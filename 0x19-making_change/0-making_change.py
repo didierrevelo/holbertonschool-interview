8db9f65adf518ad04d64cd034aa47d957601e8f5
didierrevelo <1829@holbertonschool.com>
2022-02-02 10:40:14 -0500

    0. Change comes from within

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -2,30 +2,31 @@
 
 """0. Change comes from within"""
 
+
 def makeChange(coins, total):
-  """Given a pile of coins of
-  different values, determine
-  the fewest number of coins
-  needed to meet a given amount
-  total."""
-  if(total <= 0):
-    return 0
-  
-  placeholder = total + 1
+    """Given a pile of coins of
+    different values, determine
+    the fewest number of coins
+    needed to meet a given amount
+    total."""
+    if(total <= 0):
+        return 0
 
-  memory = {0: 0}
+    placeholder = total + 1
 
-  for i in range(1, total + 1):
-      memory[i] = placeholder
+    memory = {0: 0}
 
-      for coin in coins:
-          current = i - coin
-          if current < 0:
-              continue
+    for i in range(1, total + 1):
+        memory[i] = placeholder
 
-          memory[i] = min(memory[current] + 1, memory[i])
+    for coin in coins:
+        current = i - coin
+        if current < 0:
+            continue
 
-  if memory[total] == total + 1:
-      return -1
+    memory[i] = min(memory[current] + 1, memory[i])
 
-  return memory[total]
+    if memory[total] == total + 1:
+        return -1
+
+    return memory[total]
